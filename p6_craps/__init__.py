a9f99586faca845127098948fffd595a2a0d2f03
Philip M. Gollucci <pgollucci@p6m7g8.com>
2025-11-23 21:18:14 -0500

    feat(sim): add simulation loop with points and bankroll limits (#6)

--- p6_craps/__init__.py
+++ p6_craps/__init__.py
@@ -3,6 +3,13 @@
 from . import config as config_module
 from . import engine as engine_module
 from . import players as players_module
+from . import sim as sim_module  # new
 from . import strategy as strategy_module
 
-__all__ = ["config_module", "engine_module", "players_module", "strategy_module"]
+__all__ = [
+    "config_module",
+    "engine_module",
+    "players_module",
+    "strategy_module",
+    "sim_module",
+]
